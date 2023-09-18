506ada726ae156a8f296cad9d6ae91e3d5541ac8
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-09-18 11:25:09 +0530

    Update variable.tf

--- modules/contos/platform/connectivity/key/variable.tf
+++ modules/contos/platform/connectivity/key/variable.tf
@@ -1,24 +1,136 @@
-variable "app_vault-name" {
-  type = string
+variable "key_vault_name" {
+
+    description = "name of your keyvault"
+
+    type = string
+
 }
-variable "resource-group-name" {
-  type = string
-}
-variable "location" {
-  type = string
-}
-variable "tenant_id" {
-  type = string
-}
+
 variable "soft_delete_retention_days" {
-  type = number
+
+    description = "soft delete retention days"
+
+    type = number
+
 }
+
 variable "purge_protection_enabled" {
-  type = bool
+
+    description = "enabled"
+
+    type = bool
+
 }
+
 variable "sku_name" {
-  type = string
+
+    description = "name of your sku"
+
+    type = string
+
 }
+
+variable "key_permissions" {
+
+    description = "key permissions"
+
+    type = list(string)
+
+}
+
+variable "secret_permissions" {
+
+    description = "secret permissions"
+
+    type = list(string)
+
+}
+
+variable "storage_permissions" {
+
+    description = "storage permissions"
+
+    type = list(string)
+
+}
+
+
+variable "secret1_name" {
+
+    description = "secret1_name"
+
+    type = string
+
+}
+
+variable "secret1_value" {
+
+    description = "secret1_value"
+
+    type = string
+
+}
+
+variable "secret2_name" {
+
+    description = "secret2_name"
+
+    type = string
+
+}
+
+variable "secret2_value" {
+
+    description = "secret2_value"
+
+    type = string
+
+}
+
+variable "secret3_name" {
+
+    description = "secret3_name"
+
+    type = string
+
+}
+
+variable "secret3_value" {
+
+    description = "secret3_value"
+
+    type = string
+
+}
+
+variable "secret4_name" {
+
+    description = "secret4_name"
+
+    type = string
+
+}
+
+variable "secret4_value" {
+
+    description = "secret4_value"
+
+    type = string
+
+}
+
+variable "object_id" {
+
+    description = "user id"
+
+    type = string
+  
+}
+
+variable "resource-group-name" {}
+variable "location" {}
+
+
 variable "vmpassword-name" {
   type = string
 }
