533b1634fc8ea865ad9e24d98a538a810e65742f
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-09-18 11:24:18 +0530

    Update main.tf

--- modules/contos/platform/connectivity/key/main.tf
+++ modules/contos/platform/connectivity/key/main.tf
@@ -1,36 +1,102 @@
 data "azurerm_client_config" "current" {}
 
-
 resource "azurerm_key_vault" "app_vault" {  
-  name                        = var.app_vault-name
-  location                    = var.location
-  resource_group_name         = var.resource-group-name  
-  tenant_id                   = var.tenant_id
+
+  name                        = var.key_vault_name
+
+  location                    =  var.location
+
+  resource_group_name         = var.resource-group-name
+
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+
   soft_delete_retention_days  = var.soft_delete_retention_days
+
   purge_protection_enabled    = var.purge_protection_enabled
+
   sku_name = var.sku_name
+
   access_policy {
+
     tenant_id = data.azurerm_client_config.current.tenant_id
+
     object_id = data.azurerm_client_config.current.object_id
-    key_permissions = [
-      "Get",
-    ]
-    secret_permissions = [
-      "Get", "Backup", "Delete", "List", "Purge", "Recover", "Restore", "Set",
-    ]
-    storage_permissions = [
-      "Get",
-    ]
+
+    key_permissions = var.key_permissions
+
+    secret_permissions = var.secret_permissions
+
+    storage_permissions = var.storage_permissions
+
   }
- // depends_on = [
-  //  azurerm_resource_group.app_grp
- // ]
+
+  access_policy  {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = var.object_id
+    key_permissions = var.key_permissions
+    secret_permissions = var.secret_permissions
+      
+  }
 }
 
+ 
 # We are creating a secret in the key vault
-resource "azurerm_key_vault_secret" "vmpassword" {
-  name         = var.vmpassword-name
-  value        = var.vmpassword-value
+
+resource "azurerm_key_vault_secret" "client_id" {
+
+  name         =  var.secret1_name
+
+  value        = var.secret1_value
+
   key_vault_id = azurerm_key_vault.app_vault.id
- // depends_on = [ azurerm_key_vault.app_vault ]
+
+  depends_on = [ azurerm_key_vault.app_vault ]
+
+}
+
+resource "azurerm_key_vault_secret" "client_secret" {
+
+  name = var.secret2_name
+
+  value =var.secret2_value
+
+  key_vault_id = azurerm_key_vault.app_vault.id
+
+  depends_on = [ azurerm_key_vault.app_vault ]
+
+}
+
+resource "azurerm_key_vault_secret" "tenant_id" {
+
+  name = var.secret3_name
+
+  value = var.secret3_value
+
+  key_vault_id = azurerm_key_vault.app_vault.id
+
+  depends_on = [ azurerm_key_vault.app_vault ]
+
+}
+
+resource "azurerm_key_vault_secret" "subscription_id" {
+
+  name = var.secret4_name
+
+  value = var.secret4_value
+
+  key_vault_id = azurerm_key_vault.app_vault.id
+
+  depends_on = [ azurerm_key_vault.app_vault ]
+
+}
+
+resource "azurerm_key_vault_secret" "vmpassword" {
+
+  name         = var.vmpassword-name
+
+  value        = var.vmpassword-value
+
+  key_vault_id = azurerm_key_vault.app_vault.id
+
+  depends_on = [ azurerm_key_vault.app_vault ]
 }
